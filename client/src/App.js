9c166b0ab7e4296185c61f83828bd423bd6808f9
ghayn <ghayn@outlook.com>
2019-03-03 19:28:00 +0800

    chore(client): add apollo as graphql request client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost: 2333/graphql" }),
+  cache: new InMemoryCache()
+});
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello, World
-      </div>
+      <ApolloProvider client={client}>
+        <div>
+          <h1>Reading List</h1>
+        </div>
+      </ApolloProvider>
     );
   }
 }
